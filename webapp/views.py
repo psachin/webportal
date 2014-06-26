6e262b205ace3175389a1b5a7f753f7a32a5a6e9
khushbu14 <khushbu.ag23@gmail.com>
2014-06-26 18:15:10 +0530

    correction in reviewer signup

--- webapp/views.py
+++ webapp/views.py
@@ -629,7 +629,7 @@ def reviewer_signup(request):
     context = RequestContext(request)
     registered = False
     if request.method == 'POST':
-        print "we have a request to register"
+        print "we have a request to register"    
         user_form = UserForm(data=request.POST)
         reviewer_form = ReviewerForm(data=request.POST)
         if user_form.is_valid() and reviewer_form.is_valid():
@@ -644,36 +644,30 @@ def reviewer_signup(request):
             reviewer.user = user
             if 'picture' in request.FILES:
                 reviewer.picture = request.FILES['picture']
-            reviewer.save()
+            reviewer.save()                       
             registered = True
-            email_subject = "New reviewer has registered"
-            email_message = """
+            email_subject="New reviewer has registered"
+	    email_message="""
 New reviewer has registered.
+    	
 Details:
 Name:""" + user.first_name + """  """ + user.last_name + """"
 Email:""" + user.email + """
 Waiting for your your approval"""
-            # send_mail(email_subject, email_message, 'khushbu.ag23@gmail.com',
-            #           ['pri.chundawat@gmail.com'],fail_silently=False)
-            messages.success(
-                request,
-                "form successfully submitted. Waiting for activation  from \
-admin.")
-            return HttpResponseRedirect('webapp.views.reviewer_signup')
+            #send_mail(email_subject, email_message, 'khushbu.ag23@gmail.com', ['pri.chundawat@gmail.com'],fail_silently=False)
+
+            messages.success(request,"form successfully submitted. Waiting for activation  from admin.")
+            return HttpResponseRedirect(reverse('webapp.views.reviewer_signup'))
         else:
             if reviewer_form.errors or user_form.errors:
                 print user_form.errors, reviewer_form.errors
     else:
         reviewer_form = ReviewerForm()
-        user_form = UserForm()
-    context_dict = {
-        'user_form': user_form,
-        'reviewer_from': reviewer_form,
-        'registered': registered,
-    }
-    return render_to_response('webapp/reviewer_signup.html',
-                              context_dict, context)
-
+        user_form = UserForm()	
+        context_dict = {'user_form': user_form,
+                        'reviewer_form': reviewer_form,
+                        'registered': registered}
+        return render_to_response('webapp/reviewer_signup.html', context_dict, context)
 
 def user_logout(request):
     """
